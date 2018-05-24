3da14ba05f333aeb6ee969926a86dd1c52be5c15
Mohammed Akram Faiz Ahmed <akramfaiz@Mohammeds-MacBook-Pro.local>
2018-05-24 13:56:29 +0530

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyAbua_XVH3hoxJ5Eq-HDVPwFezlgvTg3hc",
+    authDomain: "akkyspainting-d88ae.firebaseapp.com",
+    databaseURL: "https://akkyspainting-d88ae.firebaseio.com",
+    projectId: "akkyspainting-d88ae",
+    storageBucket: "",
+    messagingSenderId: "674280117209"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyAbua_XVH3hoxJ5Eq-HDVPwFezlgvTg3hc",
+    authDomain: "akkyspainting-d88ae.firebaseapp.com",
+    databaseURL: "https://akkyspainting-d88ae.firebaseio.com",
+    projectId: "akkyspainting-d88ae",
+    storageBucket: "",
+    messagingSenderId: "674280117209"
+  }
+};
