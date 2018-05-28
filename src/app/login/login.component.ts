431c25594a275d1215dfd3cdc14ddcb6d58cf7b3
Mohammed Akram Faiz Ahmed <akramfaiz@Mohammeds-MacBook-Pro.local>
2018-05-29 00:48:16 +0530

    login with google

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as fireB from 'firebase';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class LoginComponent{
+  googleProvider = new fireB.auth.GoogleAuthProvider();
+  constructor(private afAuth:AngularFireAuth) { }
+  login(){
+    this.afAuth.auth.signInWithRedirect(this.googleProvider).then(result =>{
+      if(result.user){
+        console.log(result.user);
+      }
+    });
   }
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,17 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { StaticInjector } from '@angular/core/src/di/injector';
+import * as fireB from 'firebase';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent implements OnInit {
-  username: String;
-  constructor() { }
+export class NavbarComponent{
+  userOb: Observable<fireB.User>;
+ 
+  constructor(private afAuth:AngularFireAuth) { 
+    this.userOb =  afAuth.authState;
+    console.log(afAuth.authState);
+  }
 
-  ngOnInit() {
-    this.username= "Andy";
+  logout(){
+    this.afAuth.auth.signOut();
   }
 
 }
+
+// afAuth.authState.subscribe(userState => {
+//   console.log("User State:",userState);
+//   this.username = userState.displayName;
+// })
+
+    
